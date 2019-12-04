4e756a119051a84ed8bd195ff6d106573f58e0ca
Zola Zhou <zolazhou@gmail.com>
2019-12-04 18:57:37 +0800

    add util/metrics

--- src/main/calyx/metrics.clj
+++ src/main/calyx/metrics.clj
@@ -1,1 +1,62 @@
-(ns calyx.metrics)
+(ns calyx.metrics
+  (:require [clojure.string :as str]
+            [taoensso.timbre :as log]
+            [com.stuartsierra.component :as component]
+            [metrics.core :as mc]
+            [metrics.timers :as timers]))
+
+;; Component =======================================================
+
+(defrecord Metrics []
+  component/Lifecycle
+  (start [this]
+    (log/info "Starting metrics ...")
+    (assoc this :reg (mc/new-registry)))
+  (stop [{:keys [reg] :as this}]
+    (when reg
+      (log/info "Shutdown metrics")
+      (mc/remove-all-metrics reg)
+      (dissoc this :reg))))
+
+(defn new-metrics []
+  (Metrics.))
+
+;; API/helpers =====================================================
+
+
+(defn start-timer [m n]
+  (timers/start (timers/timer (:reg m) n)))
+
+(defn stop-timer [t]
+  (timers/stop t))
+
+
+
+;; prometheus support ==============================================
+
+(defn latencies [t]
+  (->> (timers/percentiles t)
+       (map (fn [[k v]] [k (/ v 1000000)]))
+       (into {})))
+
+(defn rates [t]
+  (timers/rates t))
+
+(defn timer-metrics [n t]
+  (let [[group type title table] (str/split n #"\.")]
+    (concat
+      (map (fn [[k v]]
+             (if table
+               (format "%s:%s:request_latency_ms{method=\"%s\",table=\"%s\",quantile=\"%s\"} %.3f"
+                       group type title table k (float v))
+               (format "%s:%s:request_latency_ms{method=\"%s\",quantile=\"%s\"} %.3f"
+                       group type title k (float v))))
+           (latencies t))
+      (map (fn [[k v]]
+             (if table
+               (format "%s:%s:request_rates_qps{method=\"%s\",table=\"%s\",duration=\"%s\"} %.3f"
+                       group type title table k (float v))
+               (format "%s:%s:request_rates_qps{method=\"%s\",duration=\"%s\"} %.3f"
+                       group type title k (float v))))
+           (rates t)))))
+
--- src/main/calyx/util.clj
+++ src/main/calyx/util.clj
@@ -1,1 +1,34 @@
-(ns calyx.util)
+(ns calyx.util
+  (:import [java.security MessageDigest]
+           [java.util UUID]
+           [java.time Instant]))
+
+;; core ========================================
+
+;; time ========================================
+
+(defn timestamp []
+  (.getEpochSecond (Instant/now)))
+
+;; random ======================================
+
+(defn md5 [v]
+  (let [^bytes data      (if (bytes? v) v (.getBytes ^String v))
+        ^MessageDigest h (doto (MessageDigest/getInstance "MD5")
+                           (.update data))]
+    (.digest h)))
+
+(defn uuid []
+  (str (UUID/randomUUID)))
+
+;; ================================================
+
+(defn extract-options [args]
+  (cond
+    (nil? (first args)) nil
+    (map? (first args)) (do
+                          (assert (empty? (rest args)) "When using options map arity, function take only one parameter.")
+                          (first args))
+    :else (do
+            (assert (even? (count args)) "When using keyword args arity, there should be even number of parameters.")
+            (apply hash-map args))))
