9efefc5dc40ecf414c83b693924624ed33fc9a27
Zola Zhou <zolazhou@gmail.com>
2019-12-06 10:38:59 +0800

    增加 sha1, base64 函数

--- src/main/calyx/util.clj
+++ src/main/calyx/util.clj
@@ -1,7 +1,8 @@
 (ns calyx.util
   (:import [java.security MessageDigest]
-           [java.util UUID]
-           [java.time Instant]))
+           [java.util UUID Base64]
+           [java.time Instant]
+           [org.apache.commons.codec.binary Hex]))
 
 ;; core ========================================
 
@@ -18,8 +19,45 @@
                            (.update data))]
     (.digest h)))
 
+(defn sha1 [v]
+  (let [^bytes data      (if (bytes? v) v (.getBytes ^String v))
+        ^MessageDigest h (doto (MessageDigest/getInstance "SHA1")
+                           (.update data))]
+    (.digest h)))
+
+(defn base64-encode [v]
+  (let [^bytes data (if (bytes? v) v (.getBytes ^String v))]
+    (.encodeToString (Base64/getEncoder) data)))
+
+(defn base64-decode [^String s]
+  (.decode (Base64/getDecoder) s))
+
+(defn str->bytes
+  "Convert string to byte array."
+  ([^String s]
+   (str->bytes s "UTF-8"))
+  ([^String s, ^String encoding]
+   (.getBytes s encoding)))
+
+(defn bytes->str
+  "Convert byte array to String."
+  ([^bytes data]
+   (bytes->str data "UTF-8"))
+  ([^bytes data, ^String encoding]
+   (String. data encoding)))
+
+(defn bytes->hex
+  "Convert a byte array to hex encoded string."
+  [^bytes data]
+  (Hex/encodeHexString data))
+
+(defn hex->bytes
+  "Convert hexadecimal encoded string to bytes array."
+  [^String data]
+  (Hex/decodeHex (.toCharArray data)))
+
 (defn uuid []
-  (str (UUID/randomUUID)))
+  (UUID/randomUUID))
 
 ;; ================================================
 
