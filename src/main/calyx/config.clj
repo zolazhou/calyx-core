08e713a58f5069a48d5f110196b47cf96009d448
Zola Zhou <zolazhou@gmail.com>
2019-12-10 17:12:05 +0800

    fixing function hook

--- src/main/calyx/config.clj
+++ src/main/calyx/config.clj
@@ -2,12 +2,12 @@
   (:require [clojure.tools.logging :as log]
             [cprop.core]
             [cprop.source]
-            [taoensso.truss :refer [have]]))
+            [taoensso.truss :refer [have?]]))
 
 (defn load-config
   ([] (load-config nil))
   ([file]
-   {:pre [(have [:or nil? string?] file)]}
+   {:pre [(have? [:or nil? string?] file)]}
    (let [props (cprop.source/from-system-props)
          env   (cprop.source/from-env)
          file  (or file (:calyx-config props) (:calyx-config env))]
@@ -19,7 +19,7 @@
 (defn env-config-file
   "Get config file path from system environments"
   [key]
-  {:pre [(have [:or keyword? string?] key)]}
+  {:pre [(have? [:or keyword? string?] key)]}
   (get (cprop.source/from-env)
        (if (string? key) (keyword key) key)))
 
