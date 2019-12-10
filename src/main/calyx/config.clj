65a40480383a48ec319cdf24695415743d4c8860
Zola Zhou <zolazhou@gmail.com>
2019-12-10 13:38:22 +0800

    添加函数：config/env-config-file

--- src/main/calyx/config.clj
+++ src/main/calyx/config.clj
@@ -1,11 +1,13 @@
 (ns calyx.config
-  (:require [cprop.core]
+  (:require [clojure.tools.logging :as log]
+            [cprop.core]
             [cprop.source]
-            [clojure.tools.logging :as log]))
+            [taoensso.truss :refer [have]]))
 
 (defn load-config
   ([] (load-config nil))
   ([file]
+   {:pre [(have [:or nil? string?] file)]}
    (let [props (cprop.source/from-system-props)
          env   (cprop.source/from-env)
          file  (or file (:calyx-config props) (:calyx-config env))]
@@ -13,4 +15,11 @@
      (if (some? file)
        (cprop.core/load-config :file file :merge [props env])
        (cprop.core/load-config :resource "config.edn" :merge [props env])))))
+
+(defn env-config-file
+  "Get config file path from system environments"
+  [key]
+  {:pre [(have [:or keyword? string?] key)]}
+  (get (cprop.source/from-env)
+       (if (string? key) (keyword key) key)))
 
