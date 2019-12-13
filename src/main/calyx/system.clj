54421d9773f582c1ec00c46ab52edee4ccabcc3d
Zola Zhou <zolazhou@gmail.com>
2019-12-13 20:23:45 +0800

    Find running system

--- src/main/calyx/system.clj
+++ src/main/calyx/system.clj
@@ -1,9 +1,18 @@
-(ns calyx.system)
+(ns calyx.system
+  (:require [com.stuartsierra.component :as component]))
+
+(defn system-map [& keyvals]
+  (-> (apply component/system-map keyvals)
+      (assoc :__calyx_system__
+             (with-meta
+               {}
+               {`component/start #(assoc % :started? true)
+                `component/stop  #(dissoc % :started?)}))))
 
 (defn- find-running-system [namespace]
   (when-let [n (find-ns namespace)]
     (when-let [s (var-get (ns-resolve n 'system))]
-      (when (get-in s [:context :started?])
+      (when (get-in s [:__calyx_system__ :started?])
         s))))
 
 (defn running-system []
